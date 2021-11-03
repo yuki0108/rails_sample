369e3ec2fa544287e9cacdf85621d8b2a85bb619
先規　勇樹 <yukisaki80@gmail.com>
2021-11-03 13:20:49 +0900

    ログインレイアウトを整える

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,7 +6,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if user.save
+    if @user.save
       redirect_to admin_user_url(@user.id), notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
@@ -14,12 +14,31 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user =User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if user.update(user_params)
+      redirect_to admin_user_url(@user.id), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
   end
 
   private
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ApplicationRecord
-  has_secure_password
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
