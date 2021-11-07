320bebe82d39356b31dd2d0c1fb8fe3a42aaa31e
先規　勇樹 <yukisaki80@gmail.com>
2021-11-07 12:37:16 +0900

    created_atでソートする

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   def index
     # @tasks = current_user.tasks.order(created_at: :desc)
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true).recent
+    @tasks = @q.result(distinct: true)
   end
 
   def show
